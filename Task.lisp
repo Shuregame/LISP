8417cc99e072bf0f758b49a04555be005102c4c0
Shuregame <40614611+Shuregame@users.noreply.github.com>
2020-04-15 12:42:43 +0300

    Update Task.lisp

--- Task.lisp
+++ Task.lisp
@@ -1,41 +1,3 @@
-;9 
-;Определите функцию, разделяющую исходный список на два подсписка. 
-;В первый из них должны попасть элементы с нечетными номерами, 
-;во второй — элементы с четными номерами.
-;(1 2 3) -> ((1 3) (2))
-
-
-(defun list-sublist(lst)
-     (cond
-          ((null (cdr lst)) (list lst))
-	       (t 
-		 ((lambda (lst1)
-		    (list
-			(cons (car lst) (car lst1))
-			(cons (cadr lst) (cadr lst1))))
-		(list-sublist (cddr lst)))
-	)
-)
-)
-
-
-
-(write-line "Задача 9 Test 1")
-;;; Test 1
-(princ " >> (1 2 3) ")
-(print(list-sublist`(1 2 3)))
-(write-line "")
-(write-line "")
-
-(write-line "Задача 9 Test 1")
-;;; Test 2
-(princ " >> (1 2 3 4 5 6 7 8) ")
-(print(list-sublist`(1 2 3 4 5 6 7 8)))
-(write-line "")
-(write-line "")
-
-
-
 ; 11
 ; Определите функцию, осуществляющую разделение
 ;исходного списка на два подсписка. 
@@ -76,5 +38,44 @@
 (write-line "")
 
 (write-line "")
+
+
+
+;9 
+;Определите функцию, разделяющую исходный список на два подсписка. 
+;В первый из них должны попасть элементы с нечетными номерами, 
+;во второй — элементы с четными номерами.
+;(1 2 3) -> ((1 3) (2))
+
+
+(defun list-sublist(lst)
+     (cond
+          ((null (cdr lst)) (list lst))
+	       (t 
+		 ((lambda (lst1)
+		    (list
+			(cons (car lst) (car lst1))
+			(cons (cadr lst) (cadr lst1))))
+		(list-sublist (cddr lst)))
+	)
+)
+)
+
+
+
+(write-line "Задача 9 Test 1")
+;;; Test 1
+(princ " >> (1 2 3) ")
+(print(list-sublist`(1 2 3)))
+(write-line "")
+(write-line "")
+
+(write-line "Задача 9 Test 1")
+;;; Test 2
+(princ " >> (1 2 3 4 5 6 7 8) ")
+(print(list-sublist`(1 2 3 4 5 6 7 8)))
+(write-line "")
+(write-line "")
+
 
 
