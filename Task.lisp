71f83dd65d50ea92905cf2883b301d8564693ad2
Shuregame <40614611+Shuregame@users.noreply.github.com>
2020-04-22 12:14:56 +0300

    Update Task.lisp

--- Task.lisp
+++ Task.lisp
@@ -135,3 +135,145 @@
 ; 1
 (print (ПЕРВЫЙ-АТОМ '((+ 1 2) 5)))
 ; +
+
+;№17
+;Создайте предикат, порождающий всевозможные перестановки исходного множества.
+(defun insert-elem-in-each-position (elem list)
+	(cond
+		((null list) (list elem))
+		((atom list) (insert-elem-in-each-position elem (list list)))
+		(t (cons (cons elem list)
+			     (insert-elem-in-each-position-aux elem nil list)))
+	)
+)
+
+(defun insert-elem-in-each-position-aux (elem list1 list2)
+	(cond
+		((null list2) nil)
+		(t
+			((lambda (a)
+				(cons
+					(append (car a) (list elem) (cadr a))
+					((lambda (x)
+						(insert-elem-in-each-position-aux elem
+							(first x)
+							(second x)))
+					a))
+			)
+			((lambda (list1 list2)
+				(list (append list1 (list (car list2))) (cdr list2)))
+			list1 list2)))
+		)
+	)
+
+(defun add-elem-for-each-permutation (elem perm-lst)
+	(cond
+		((null perm-lst) nil)
+		(t (append
+				(insert-elem-in-each-position elem (car perm-lst))
+				(add-elem-for-each-permutation elem (cdr perm-lst))))
+	)
+)
+
+(defun my-permutation (lst)
+	(cond
+		((null lst) nil)
+		((null (cdr lst)) (list lst))
+		(t (add-elem-for-each-permutation
+			(car lst)
+			(my-permutation (cdr lst))))
+	)
+)
+
+(print (my-permutation '(1 2 3)))
+
+;№46
+;Предположим, что отец и мать некоторого лица, хранятся как значения соответствующих свойств у символа, обозначающего это лицо. 
+;Напишите функцию (РОДИТЕЛИ x), которая возвращает в качестве значения родителей, и предикат (СЕСТРЫ-БРАТЬЯ x1 x2),
+;который истинен в случае, если x1 и x2 — сестры или братья, родные или с одним общим родителем.
+( setf ( get 'C 'mother) 'A)
+( setf ( get 'C 'father) 'B)
+
+( setf ( get 'H 'mother) 'E)
+( setf ( get 'H 'father) 'R)
+
+( setf ( get 'K 'mother) 'M)
+( setf ( get 'K 'father) 'N)
+
+( setf ( get 'Q 'mother) 'M)
+( setf ( get 'Q 'father) 'N)
+
+(defun get-mother(x)
+(get x 'mother)
+)
+
+(defun get-father(x)
+(get x 'father)
+)
+
+(defun parents(x)
+(list (get-mother x) (get-father x))
+)
+
+(defun sisters-brothers(x1 x2)
+    (cond
+        ((STRING= (get x1 'father) (get x2 'father))t)
+        ((STRING= (get x1 'mother) (get x2 'mother))t)
+        (t nil)
+    )
+)
+
+; A  B      E  R      M  N
+;  \/        \/        \/
+;  С         H        K  Q
+
+
+(print(parents 'C))
+(print(parents 'H))
+(print(parents 'Q))
+(print(parents 'K))
+
+(print(sisters-brothers 'H 'K))
+(print(sisters-brothers 'K 'Q))
+
+; 45 Задача
+; Определить расстояние между двумя точками. У символа (названия города) определены свойства x и y,
+; необходимо вычислить расстояние между двумя городами.
+; struct city {
+;   float x, y;
+; }
+; length_city (city, city) -> float
+
+(defun length_city (city-a city-b)
+    (sqrt (+
+        (expt
+            (- (get city-a 'x) (get city-b 'x))
+        2)
+        (expt
+            (- (get city-a 'y) (get city-b 'y))
+        2)
+    ))
+)
+(defun distance-city (name x y)
+        (setf (get name 'x) x)
+        (setf (get name 'y) y)
+)
+
+
+; Test 1
+(write-line "Задача 45 Test 1")
+(princ ">> {0, 5} {7, 0}")
+(distance-city 'city1 0 5)
+(distance-city 'city2 7 0)
+(print (length_city 'city1 'city2))
+(write-line "")
+(write-line "")
+
+; Test 2
+(write-line "Задача 45 Test 2")
+(princ ">> {32, 19} {58, 26}")
+(distance-city 'city1 32 19)
+(distance-city 'city2 58 26)
+(print (length_city 'city1 'city2))
+(write-line "")
+(write-line "")
